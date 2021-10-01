2ce685f32ada69dd7e66157164dc6a7e8fcb4073
Yacine22 <40285314+Yacine22@users.noreply.github.com>
2021-10-01 16:54:38 +0200

    Update Communication parameters
    Add a Serial Communication Protocol based on 2 wires

--- Recepteur_V3/Recepteur_V3.ino
+++ Recepteur_V3/Recepteur_V3.ino
@@ -3,6 +3,11 @@
 //EDIT 23 SEPT 2021 <- debut opto 3 et 4
 */
 
+/**
+  //EDIT 1er Juin 2021
+  //EDIT 23 SEPT 2021 <- debut opto 3 et 4
+*/
+
 #include <VirtualWire.h>
 
 /** Broches pour les LEDs */
@@ -23,80 +28,14 @@ const char* CMD_TEST = "test";
 
 
 unsigned long previousMillis = 0;
-long tpsattente = 1000; 
+long tpsattente = 1000;
 int cpttps = 0;
 int cpttpsmax = 60;
 int cpt = 1;
 
-void setup() {
-  //Serial.begin(9600);
-  //Serial.println("SALUT MA GUEULE");
-  /* Met les broches des LEDs en sortie et à LOW */
-  pinMode(opto1, OUTPUT);
-  pinMode(opto2, OUTPUT);
-  pinMode(opto3, OUTPUT);
-  pinMode(opto4, OUTPUT);
-//  pinMode(RF, INPUT);
-  pinMode(Buzzer, OUTPUT);
-  pinMode(PIN_LED_R, OUTPUT);
-  pinMode(PIN_LED_G, OUTPUT);
-  pinMode(PIN_LED_B, OUTPUT);
-  pinMode(recep, INPUT);
-  digitalWrite(opto1, LOW);
-  digitalWrite(opto2, LOW);
-  digitalWrite(opto3, LOW);
-  digitalWrite(opto4, LOW);
-  digitalWrite(Buzzer, LOW);
-
-  digitalWrite(PIN_LED_R, LOW);
-  digitalWrite(PIN_LED_G, LOW);
-  digitalWrite(PIN_LED_B, HIGH);
-
-
-
-  // Initialisation de la bibliothèque VirtualWire
-  // Vous pouvez changez les broches RX/TX/PTT avant vw_setup() si nécessaire
-  previousMillis = millis();
-}
-
-void loop() {
-  // On attend de recevoir un message
-  //vw_wait_rx();
-  //vw_wait_rx_max(60000);
-  if (millis() - previousMillis >= tpsattente)
-  {
-    previousMillis = millis();
-    cpttps++;
-    flashgreen();
-    digitalWrite(PIN_LED_R, 0);
-    digitalWrite(PIN_LED_G, 0);
-    digitalWrite(PIN_LED_B, 0);
-    if (cpttps >= cpttpsmax)
-    {
-      cpttps = 0;
-      BEEP();
-    }
-  }
-
-  if (digitalRead(recep))
-  { //Serial.println("MANUAL OVERRIDE");
-    digitalWrite(PIN_LED_R, LOW);
-    digitalWrite(PIN_LED_G, HIGH);
-    digitalWrite(PIN_LED_B, HIGH);
-    takephoto();
-    beep();
-
-    digitalWrite(PIN_LED_R, LOW);
-    digitalWrite(PIN_LED_G, LOW);
-    digitalWrite(PIN_LED_B, LOW);
-    cpttps = 0;
-  }
-}
-
-
-void takephoto()
+void takephoto2cameras()
 {
-  
+
   digitalWrite(opto1, 0);
   digitalWrite(opto2, 0);
   digitalWrite(opto3, 0);
@@ -113,10 +52,50 @@ void takephoto()
   digitalWrite(opto3, 0);
   digitalWrite(opto4, 0);
   delay(20);
-  
+
 }
+
+void takephoto1camera1()
+{
+
+  zeroOpto();
+  digitalWrite(opto1, 1);
+
+  delay(50);
+  digitalWrite(opto2, 1);
+
+  delay(100);
+  zeroOpto();
+}
+
+
+
+void takephoto1camera2()
+{
+
+  zeroOpto();
+  digitalWrite(opto3, 1);
+
+  delay(50);
+  digitalWrite(opto4, 1);
+
+  delay(100);
+  zeroOpto();
+
+}
+
+void zeroOpto() {
+  digitalWrite(opto1, 0);
+  digitalWrite(opto2, 0);
+  digitalWrite(opto3, 0);
+  digitalWrite(opto4, 0);
+  delay(50);
+}
+
+
+
 void BEEP() {
- // Serial.println("YOUHOU YA QUELQUUN?");
+  // Serial.println("YOUHOU YA QUELQUUN?");
   for (int i = 0; i < 5; i++)
   {
     digitalWrite(PIN_LED_R, HIGH);
@@ -169,3 +148,126 @@ void mario2()
   tone(Buzzer, 1520, 100);
   delay(575);
 }
+
+void setup() {
+  //Serial.begin(9600);
+  //Serial.println("SALUT MA GUEULE");
+  /* Met les broches des LEDs en sortie et à LOW */
+  pinMode(opto1, OUTPUT);
+  pinMode(opto2, OUTPUT);
+  pinMode(opto3, OUTPUT);
+  pinMode(opto4, OUTPUT);
+  //  pinMode(RF, INPUT);
+  pinMode(Buzzer, OUTPUT);
+  pinMode(PIN_LED_R, OUTPUT);
+  pinMode(PIN_LED_G, OUTPUT);
+  pinMode(PIN_LED_B, OUTPUT);
+  pinMode(recep, INPUT);
+  digitalWrite(opto1, LOW);
+  digitalWrite(opto2, LOW);
+  digitalWrite(opto3, LOW);
+  digitalWrite(opto4, LOW);
+  digitalWrite(Buzzer, LOW);
+
+  digitalWrite(PIN_LED_R, LOW);
+  digitalWrite(PIN_LED_G, LOW);
+  digitalWrite(PIN_LED_B, HIGH);
+
+
+
+  // Initialisation de la bibliothèque VirtualWire
+  // Vous pouvez changez les broches RX/TX/PTT avant vw_setup() si nécessaire
+  previousMillis = millis();
+}
+
+void loop() {
+  // On attend de recevoir un message
+  //vw_wait_rx();
+  //vw_wait_rx_max(60000);
+  if (millis() - previousMillis >= tpsattente)
+  {
+    previousMillis = millis();
+    cpttps++;
+    flashgreen();
+    digitalWrite(PIN_LED_R, 0);
+    digitalWrite(PIN_LED_G, 0);
+    digitalWrite(PIN_LED_B, 0);
+    if (cpttps >= cpttpsmax)
+    {
+      cpttps = 0;
+      BEEP();
+    }
+  }
+  if (digitalRead(recep) == 1) {
+    
+    if (digitalRead(recep) == 0) {
+      delay(12);
+      if (digitalRead(recep) == 0)
+      { //Serial.println("MANUAL OVERRIDE");
+        digitalWrite(PIN_LED_R, LOW);
+        digitalWrite(PIN_LED_G, HIGH);
+        digitalWrite(PIN_LED_B, 0);
+        //Mario();
+        delay(2000);
+        // takephoto2cameras();
+        // beep();
+
+        digitalWrite(PIN_LED_R, LOW);
+        digitalWrite(PIN_LED_G, LOW);
+        digitalWrite(PIN_LED_B, LOW);
+        cpttps = 0;
+      }
+    }
+
+    ////
+
+    if (digitalRead(recep) == 0 ) {
+      delay(12);
+      if (digitalRead(recep) == 1 )
+      { //Serial.println("MANUAL OVERRIDE");
+        digitalWrite(PIN_LED_R, HIGH);
+        digitalWrite(PIN_LED_G, LOW);
+        digitalWrite(PIN_LED_B, LOW);
+        takephoto1camera1();
+        // beep();
+
+        digitalWrite(PIN_LED_R, LOW);
+        digitalWrite(PIN_LED_G, LOW);
+        digitalWrite(PIN_LED_B, LOW);
+        cpttps = 0;
+      }
+    }
+
+
+    if (digitalRead(recep) == 1 ) {
+      delay(12);
+      if (digitalRead(recep) == 0 )
+      { //Serial.println("MANUAL OVERRIDE");
+        digitalWrite(PIN_LED_R, LOW);
+        digitalWrite(PIN_LED_G, 1);
+        digitalWrite(PIN_LED_B, 0);
+        takephoto1camera2();
+        beep();
+
+        digitalWrite(PIN_LED_R, LOW);
+        digitalWrite(PIN_LED_G, LOW);
+        digitalWrite(PIN_LED_B, LOW);
+        // cpttps = 0;
+      }
+      else //11
+      {
+        takephoto2cameras();
+        digitalWrite(PIN_LED_R, 1);
+        digitalWrite(PIN_LED_G, 1);
+        digitalWrite(PIN_LED_B, 1);
+        // beep();
+        // beep();
+        // beep();
+
+      }
+    }
+
+  }
+}
+
+
