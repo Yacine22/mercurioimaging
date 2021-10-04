94f92fbd3591b561f47f29a71ddb72680d161319
Yacine22 <40285314+Yacine22@users.noreply.github.com>
2021-10-04 22:11:53 +0200

    Update Serial Communication
    SERIAL COMMUNICATION OPTIMIZED

--- Recepteur_V3/Recepteur_V3.ino
+++ Recepteur_V3/Recepteur_V3.ino
@@ -1,11 +1,13 @@
 /**
-//EDIT 1er Juin 2021
-//EDIT 23 SEPT 2021 <- debut opto 3 et 4
+  //EDIT 1er Juin 2021
+  //EDIT 23 SEPT 2021 <- debut opto 3 et 4
 */
 
 /**
   //EDIT 1er Juin 2021
   //EDIT 23 SEPT 2021 <- debut opto 3 et 4
+  
+  // EDITED 4 OCT 2021 / SERIAL COMMUNICATION OPTIMIZED
 */
 
 #include <VirtualWire.h>
@@ -21,6 +23,10 @@ const byte PIN_LED_R = 4;
 const byte PIN_LED_G = 3;
 const byte PIN_LED_B = 2;
 const byte recep = 6;
+
+/// added !
+int data[4];
+#define T 10
 /** Différents messages de commande */
 
 const char* CMD_CLICK = "clic";
@@ -33,13 +39,16 @@ int cpttps = 0;
 int cpttpsmax = 60;
 int cpt = 1;
 
+void clearData(){
+  for (int i=0; i<4; i++){
+    data[i] = 0;
+  }
+}
+
 void takephoto2cameras()
 {
 
-  digitalWrite(opto1, 0);
-  digitalWrite(opto2, 0);
-  digitalWrite(opto3, 0);
-  digitalWrite(opto4, 0);
+  zeroOpto();
   delay(200);
   digitalWrite(opto1, 1);
   digitalWrite(opto3, 1);
@@ -47,10 +56,7 @@ void takephoto2cameras()
   digitalWrite(opto2, 1);
   digitalWrite(opto4, 1);
   delay(100);
-  digitalWrite(opto1, 0);
-  digitalWrite(opto2, 0);
-  digitalWrite(opto3, 0);
-  digitalWrite(opto4, 0);
+  zeroOpto();
   delay(20);
 
 }
@@ -149,8 +155,23 @@ void mario2()
   delay(575);
 }
 
+float moySignCom() {
+  uint8_t data[4];
+  float sum = 0;
+
+  for (int i = 0; i < 4; i++) {
+    data[i] = digitalRead(recep);
+    delay(9);
+  }
+  for (int i = 0; i < 4; i++) {
+    sum += (float)data[i];
+  }
+  return sum / 4;
+}
+
+
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   //Serial.println("SALUT MA GUEULE");
   /* Met les broches des LEDs en sortie et à LOW */
   pinMode(opto1, OUTPUT);
@@ -181,6 +202,8 @@ void setup() {
 }
 
 void loop() {
+  Serial.println(digitalRead(recep));
+  delay(10);
   // On attend de recevoir un message
   //vw_wait_rx();
   //vw_wait_rx_max(60000);
@@ -198,76 +221,55 @@ void loop() {
       BEEP();
     }
   }
-  if (digitalRead(recep) == 1) {
-    
-    if (digitalRead(recep) == 0) {
-      delay(12);
-      if (digitalRead(recep) == 0)
-      { //Serial.println("MANUAL OVERRIDE");
-        digitalWrite(PIN_LED_R, LOW);
-        digitalWrite(PIN_LED_G, HIGH);
-        digitalWrite(PIN_LED_B, 0);
-        //Mario();
-        delay(2000);
-        // takephoto2cameras();
-        // beep();
 
-        digitalWrite(PIN_LED_R, LOW);
-        digitalWrite(PIN_LED_G, LOW);
-        digitalWrite(PIN_LED_B, LOW);
-        cpttps = 0;
-      }
+  if (digitalRead(recep) == 1){
+     data[0] = 1;
+     delay(11);
+     for (int i = 0; i < 3; i++) {
+      data[i+1] = digitalRead(recep);
+      delay(11); // Pour cette fonction ça ira ! dès qu'on aura 10 valeurs ça va être changé (10*i + 5) <=> T*(i+1) + T/2
     }
-
-    ////
-
-    if (digitalRead(recep) == 0 ) {
-      delay(12);
-      if (digitalRead(recep) == 1 )
-      { //Serial.println("MANUAL OVERRIDE");
-        digitalWrite(PIN_LED_R, HIGH);
-        digitalWrite(PIN_LED_G, LOW);
-        digitalWrite(PIN_LED_B, LOW);
-        takephoto1camera1();
-        // beep();
-
-        digitalWrite(PIN_LED_R, LOW);
-        digitalWrite(PIN_LED_G, LOW);
-        digitalWrite(PIN_LED_B, LOW);
-        cpttps = 0;
-      }
-    }
-
-
-    if (digitalRead(recep) == 1 ) {
-      delay(12);
-      if (digitalRead(recep) == 0 )
-      { //Serial.println("MANUAL OVERRIDE");
-        digitalWrite(PIN_LED_R, LOW);
-        digitalWrite(PIN_LED_G, 1);
-        digitalWrite(PIN_LED_B, 0);
-        takephoto1camera2();
-        beep();
-
-        digitalWrite(PIN_LED_R, LOW);
-        digitalWrite(PIN_LED_G, LOW);
-        digitalWrite(PIN_LED_B, LOW);
-        // cpttps = 0;
-      }
-      else //11
-      {
-        takephoto2cameras();
-        digitalWrite(PIN_LED_R, 1);
-        digitalWrite(PIN_LED_G, 1);
-        digitalWrite(PIN_LED_B, 1);
-        // beep();
-        // beep();
-        // beep();
-
-      }
-    }
-
   }
+
+  if (data[0] == 1 && data[3] == 1) {
+
+    if (data[1] == 0 && data[2] == 0) {
+      Serial.println(String(data[1]) + " " + String(data[2]));
+      mario2();
+    }
+
+    else if (data[1] == 0 && data[2] == 1) {
+      Serial.println(String(data[1]) + " " + String(data[2]));
+      takephoto1camera1();
+      beep();
+      
+      digitalWrite(PIN_LED_R, 0);
+      digitalWrite(PIN_LED_G, 0);
+      digitalWrite(PIN_LED_B, 1);
+
+    }
+
+    else if (data[1] == 1 && data[2] == 0) {
+      Serial.println(String(data[1]) + " " + String(data[2]));
+      
+      takephoto1camera2();
+      beep();
+
+      digitalWrite(PIN_LED_R, 1);
+      digitalWrite(PIN_LED_G, 0);
+      digitalWrite(PIN_LED_B, 0);
+  
+
+    }
+
+    else if (data[1] == 1 && data[2] == 1) {
+      Serial.println(String(data[1]) + " " + String(data[2]));
+
+      takephoto2cameras();
+      BEEP();
+    }
+  }
+
+  clearData(); // Turn data to Zero !!! 
+
 }
-
-
